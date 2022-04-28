16eb0626ff6de300b1a8f4767f96d6aebe34a85e
Mauricio Cevallos <mceval2@uic.edu>
2022-04-28 17:00:28 -0500

    first dream

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-console.log(navigator);
+let device;
+
+var delayInMilliseconds = 20000; //20 seconds
+
+setTimeout(function() {
+  //your code to be executed after 1 second
+  location = "index.html";
+}, delayInMilliseconds);
 
 if(navigator.requestMIDIAccess){
     navigator.requestMIDIAccess().then(success,failure);
@@ -15,6 +22,9 @@ function updateDevices(event){
 function success(midiAccess){
     midiAccess.addEventListener('statechange', updateDevices);
     const inputs = midiAccess.inputs;
+    for (var output of midiAccess.outputs.values()){
+        device = output;
+    }
     inputs.forEach((input) => {
         console.log(input);
         input.addEventListener('midimessage', handleInput);
@@ -22,6 +32,45 @@ function success(midiAccess){
     })
 
     //console.log(inputs);
+    colorAllBlue();
+    colorKeys(60, 74);
+    colorKeys(62, 74);
+    colorKeys(57, 74);
+    colorKeys(58, 74);
+    colorKeys(59, 74);
+    colorKeys(88, 74);
+    colorKeys(98, 74);
+    colorKeys(93, 74);
+    colorKeys(95, 74);
+    colorKeys(90, 74);
+    colorKeys(89, 74);
+    colorKeys(55, 74);
+    colorKeys(51, 74);
+    colorKeys(80, 74);      
+    colorKeys(84, 74);
+    colorKeys(47, 3);
+    colorKeys(76, 3);
+    colorKeys(37, 74);
+    colorKeys(38,74);
+    colorKeys(39,74);
+    colorKeys(68, 74);
+    colorKeys(69, 74);
+    colorKeys(70, 74);
+    colorKeys(75, 74);
+    colorKeys(40, 74);
+    colorKeys(45, 74);
+    colorKeys(78, 74);
+    colorKeys(82, 74);
+    colorKeys(49, 74);
+    colorKeys(50, 74);
+    colorKeys(81, 74);
+    colorKeys(53, 74);
+    colorKeys(86, 74);
+    colorKeys(79, 74);
+    colorKeys(44, 74);
+    colorKeys(65, 74);
+    
+    
 }
 
 function handleInput(input){
@@ -29,8 +78,6 @@ function handleInput(input){
     const command = input.data[0];
     const note = input.data[1];
     const velocity = input.data[2];
-
-    console.log(`command ${command} , note: ${note}, velocity: ${velocity}`);
 
     switch (command) {
         case 144:
@@ -43,16 +90,53 @@ function handleInput(input){
     }
 }
 
+// functions for button pressed :
 function noteOn(note){
     console.log(`note:${note} //on`);
-    if (note == 99){
-        document.getElementById("testelm").innerHTML = "( ͡° ͜ʖ ͡°)";
+   
+    if (note == 88){
+        location = "cheese.html";
     }
+
+    if (note == 59){
+        location = "moldy.html";
+    }
+
 }
 
 function noteOff(note){
-    console.log(`note:${note} //off`)
-    if (note == 99){
-        document.getElementById("testelm").innerHTML = "";
+   
+    if (note == 88){
+        setTimeout();
+        location = "index.html";
     }
+
+}
+
+
+function colorKeys(key, clr){
+    device && device.send([0x90,key,clr]);
+}
+
+function clearAll(){
+    for (let i = 0; i < 100; i++){
+        colorKeys(i, 0);
+    }
+}
+
+function colorAll(){
+    for(let i = 0; i < 100; i++){
+        colorKeys(i,i);
+    }
+}
+
+function colorAllBlue(){
+    for(let i = 0; i < 100; i++){
+        colorKeys(i, 67);
+    }
+}
+
+function colorNote(note){
+    
+    colorKeys(note, 74);
 }
